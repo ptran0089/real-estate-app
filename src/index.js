30288aa4b209d974f6ef872fe3c269dd9b2ab0fd
Phuong Tran <ptran0089@gmail.com>
2017-12-01 14:09:43 -0800

    Save local changes

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Match, Miss } from 'react-router';
 
 import App from './components/App';
 import notFound from './components/notFound';
 import './css/main.css';
 
-const Root = () => {
-	return (
-		<BrowserRouter basename="/">
-			<div>
-				<Match exactly pattern="/" component={App} />
-				<Miss component={notFound} />
-			</div>
-		</BrowserRouter>
-	)
-}
+// const Root = () => {
+// 	return (
+// 		<BrowserRouter basename="/">
+// 			<div>
+// 				<Match exactly pattern="/" component={App} />
+// 				<Miss component={notFound} />
+// 			</div>
+// 		</BrowserRouter>
+// 	)
+// }
 
-render(<Root />, document.querySelector('#main'));
+render(<App />, document.querySelector('#main'));
